a010f3b9f9db4e4cde09d86ed233d9a4e87c4a1a
Tyler <tpedora@me.com>
2019-09-07 09:26:14 -0600

    Finished Auth Flow

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
+
+mongoose
+  .connect(keys.mongoURI, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Connected To Database");
+  })
+  .catch(err => {
+    console.log("Error Connecting", err);
+  });
+
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+require("./routes/authRoutes")(app);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
